c39ca98baee049cbadab136dba62fbb9be682229
Vicky McDermott <vickymmcd@gmail.com>
2018-01-27 17:18:24 -0500

    finished code and got it to pass all tests

--- datastructures/day01/README.md
+++ datastructures/day01/README.md
@@ -1,4 +1,4 @@
-# `ArrayLists`
+uu# `ArrayLists`
 
 ## Learning Goals
 
--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -4,39 +4,97 @@ public class MyArrayList {
 
     // TODO: Runtime: O(?)
     public MyArrayList() {
-        // TODO
+        elems = new Cow[10];
+        size = 0;
     }
 
     // TODO: Runtime: O(?)
     public MyArrayList(int capacity) {
-        // TODO
+        elems = new Cow[capacity];
+        size = 0;
     }
 
     // TODO: Runtime: O(?)
     public void add(Cow c) {
-        // TODO
+        if(size==elems.length){
+            grow();
+        }
+        elems[size] = c;
+        size++;
     }
 
     // TODO: Runtime: O(?)
     public int size() {
-        // TODO
-        return -1;
+        return size;
     }
 
     // TODO: Runtime: O(?)
     public Cow get(int index) {
-        // TODO
-        return null;
+        if(elems[index] == null){
+            throw new IndexOutOfBoundsException("Sorry, there is no cow at that index");
+        }
+        return elems[index];
     }
 
     // TODO: Runtime: O(?)
     public Cow remove(int index) {
-        // TODO
-        return null;
+        if(index >= elems.length || index < 0){
+            throw new IndexOutOfBoundsException("Sorry, that is not a valid index");
+        }
+        else if(elems[index] == null){
+            throw new IndexOutOfBoundsException("Sorry, there is no cow at that index");
+        }
+        Cow removed = elems[index];
+        elems[index] = null;
+        if(index+1 < size){
+            for(int i=index+1; i<size;i++){
+                elems[i-1] = elems[i];
+            }
+        }
+        size = size-1;
+        if(size < .25*elems.length && size >= 4){
+            shrink();
+        }
+        return removed;
+    }
+
+    public void shrink(){
+        Cow[] B = new Cow[(int)(.5*elems.length)];
+        System.arraycopy(elems,0, B, 0, size);
+        elems = B;
     }
 
     // TODO: Runtime: O(?)
     public void add(int index, Cow c) {
-        // TODO
+        if(size == elems.length){
+            grow();
+        }
+        else if(index > size || index < 0){
+            throw new IndexOutOfBoundsException("Sorry, invalid index for addition of cow");
+        }
+        if(index+1 < size){
+            Cow temp = elems[index];
+            for(int i=index+1; i<size; i++){
+                if(i-1 == index){
+                    elems[i-1] = c;
+                }
+                else {
+                    elems[i-1] = temp;
+                    temp = elems[i];
+                }
+            }
+            size++;
+        }
+        else{
+            elems[index+1] = elems[index];
+            elems[index] = c;
+            size++;
+        }
+    }
+
+    public void grow(){
+        Cow[] B = new Cow[size*2];
+        System.arraycopy(elems,0, B, 0, size);
+        elems = B;
     }
 }
