3804c0010c63a717bab6503209b5fa2076635db3
Vicky McDermott <vickymmcd@gmail.com>
2018-03-04 22:30:58 -0500

    finishing up trees hw

--- trees/day00/src/Problems.java
+++ trees/day00/src/Problems.java
@@ -5,38 +5,24 @@ public class Problems {
     public static BinarySearchTree<Integer> minimalHeight(List<Integer> values) {
         // TODO
         Collections.sort(values); // this will be O(nlogn) but I don't think we can do better anyway
-        return minimalHeightRecursive(null, null, values, 0, values.size()-1);
+        return minimalHeightRecursive(new BinarySearchTree<>(), values, 0, values.size()-1);
     }
 
-    private static BinarySearchTree<Integer> minimalHeightRecursive(BinarySearchTree<Integer> bst, TreeNode<Integer> currRoot,
+    private static BinarySearchTree<Integer> minimalHeightRecursive(BinarySearchTree<Integer> bst,
                                                                     List<Integer> values, int min, int max){
 
-        if(values.size()==1){
-            bst = new BinarySearchTree<>();
-            bst.root = new TreeNode<>(values.get(0));
-            return bst;
-        }
         if(min>max){
             return bst;
         }
 
 
-        int mid = (values.get(max)-values.get(min))/2;
+        int midindex = ((max-min)/2)+min;
+        int mid = values.get(midindex);
 
-        if(bst== null) {
-            bst = new BinarySearchTree<>();
-            bst.root = new TreeNode<>(values.get(mid));
-            currRoot = bst.root;
-        }
-        else{
-            currRoot.key = values.get(mid);
-        }
+        bst.add(mid);
 
-        currRoot.leftChild = new TreeNode<>(0);
-        currRoot.rightChild = new TreeNode<>(0);
-
-        currRoot.leftChild = minimalHeightRecursive(bst, currRoot.leftChild, values, min, mid).root;
-        currRoot.rightChild = minimalHeightRecursive(bst, currRoot.rightChild, values, mid+1, max).root;
+        minimalHeightRecursive(bst, values, min, midindex-1);
+        minimalHeightRecursive(bst, values, midindex+1, max);
 
         return bst;
     }
