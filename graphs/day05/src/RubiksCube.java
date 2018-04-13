7915934391d5142db2fe45df03e8e41bc263e281
Vicky McDermott <vickymmcd@gmail.com>
2018-04-13 15:43:15 -0400

    updating

--- graphs/day05/src/RubiksCube.java
+++ graphs/day05/src/RubiksCube.java
@@ -4,6 +4,13 @@ import java.util.concurrent.ThreadLocalRandom;
 // use this class if you are designing your own Rubik's cube implementation
 public class RubiksCube {
 
+    Face f0;
+    Face f1;
+    Face r0;
+    Face r1;
+    Face u0;
+    Face u1;
+
     /**
      * Class to represent Cubie
      */
@@ -58,23 +65,24 @@ public class RubiksCube {
         Cubie cubie2 = new Cubie("yellow", "red", "green");
         Cubie cubie3 = new Cubie("yellow", "orange", "blue");
         Cubie cubie4 = new Cubie("yellow", "red", "blue");
-        Face f0 = new Face(cubie1, 0, cubie2, 0, cubie3,
+        f0 = new Face(cubie1, 0, cubie2, 0, cubie3,
                 0, cubie4, 0, "f0");
 
-        Cubie cubie5 = new Cubie("white", "red", "blue");
-        Cubie cubie6 = new Cubie("white", "orange", "blue");
-        Cubie cubie7 = new Cubie("white", "red", "green");
-        Cubie cubie8 = new Cubie("white", "orange", "green");
-        Face f1 = new Face(cubie5, 0, cubie6, 0, cubie7,
+        Cubie cubie5 = new Cubie("white", "orange", "blue");
+        Cubie cubie6 = new Cubie("white", "red", "blue");
+        Cubie cubie7 = new Cubie("white", "orange", "green");
+        Cubie cubie8 = new Cubie("white", "red", "green");
+
+        f1 = new Face(cubie5, 0, cubie6, 0, cubie7,
                 0, cubie8, 0, "f1");
 
-        Face r0 = new Face(cubie2, 1, cubie7, 1, cubie4, 1, cubie5, 1, "r0");
+        r0 = new Face(cubie2, 1, cubie7, 1, cubie4, 1, cubie5, 1, "r0");
 
-        Face r1 = new Face(cubie8, 1, cubie1, 1, cubie6, 1, cubie3, 1, "r1");
+        r1 = new Face(cubie8, 1, cubie1, 1, cubie6, 1, cubie3, 1, "r1");
 
-        Face u0 = new Face(cubie8, 2, cubie7, 2, cubie1, 2, cubie2, 2, "u0");
+        u0 = new Face(cubie8, 2, cubie7, 2, cubie1, 2, cubie2, 2, "u0");
 
-        Face u1 = new Face(cubie3, 2, cubie4, 2, cubie6, 2, cubie5, 2, "u1");
+        u1 = new Face(cubie3, 2, cubie4, 2, cubie6, 2, cubie5, 2, "u1");
 
 
     }
@@ -130,6 +138,16 @@ public class RubiksCube {
     // Do not modify this rubik's cube.
     public RubiksCube rotate(char c) {
         // TODO
+        if(c=='u'){
+            Cubie temp = u0.cubie1;
+            Cubie temp2 = u0.cubie2;
+            u0.cubie1 = u0.cubie3;
+            u0.cubie2 = temp;
+            temp = u0.cubie4;
+            u0.cubie4 = temp2;
+            u0.cubie3 = temp;
+        }
+
         return this;
     }
 
