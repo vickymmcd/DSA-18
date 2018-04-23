dd8756d97b8f838730febe6fc79a633708fa33da
Vicky McDermott <vickymmcd@gmail.com>
2018-04-23 16:44:41 -0400

    done with DP problems

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,33 @@
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // subproblem: DP[i] = how many dice roll sequences there are that reach sum N
+    // guess: what you rolled next? 1, 2, 3, 4, 5, 6?
+    // recurrence relation: DP[sum] = DP[sum-1]...+ DP[sum-6]
+    // recurse/memoize: store DP[i] in the memo
+    // solve original problem:return DP[i]
+
+    private static int diceRollRecursive(int sum, int[] DP){
+        if(sum==0) return 1;
+        if(sum<0) return 0;
+
+        if(DP[sum] != -1) return DP[sum];
+
+        DP[sum] = diceRollRecursive(sum-1, DP) + diceRollRecursive(sum-2, DP) +
+                diceRollRecursive( sum-3, DP) + diceRollRecursive( sum-4, DP) +
+                diceRollRecursive( sum-5, DP) + diceRollRecursive( sum-6, DP);
+
+        return DP[sum];
+    }
+
+    // Runtime: O(N*6)
+    // Space: O(N)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        int[] DP = new int[N+1];
+        for(int i=0; i< DP.length; i++){
+                DP[i] = -1;
+            }
+
+        return diceRollRecursive(N, DP);
     }
 
 }
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,49 @@
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    private static int maxLIS;
+    // subproblem: DP[i] = max LIS up to index i
+    // guess: local max is next+1 or not
+    // recurrence relation: DP[i] = DP[i-1] + 1 if DP[i] is greater
+    // recurse/memoize: store DP[i] into memo
+    // solve original problem: return DP[i]
+
+    private static int LISRecursive(int[] A, int[] DP, int n){
+        if(n<1) return 0;
+        if(n==1) return 1;
+
+        if(DP[n-1] != -1) return DP[n-1];
+
+        int localMaxLIS = 1;
+        int res = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            res = LISRecursive(A, DP, i);
+            if (A[i-1] < A[n-1] && res + 1 > localMaxLIS)
+                localMaxLIS = res + 1;
+        }
+
+        if (maxLIS < localMaxLIS)
+            maxLIS = localMaxLIS;
+
+        DP[n-1] = localMaxLIS;
+
+        return localMaxLIS;
+
+    }
+
+    // Runtime: O(N^2)
+    // Space: O(N)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        maxLIS = 0;
+
+        int[] DP = new int[A.length];
+        for(int i=0; i< DP.length; i++){
+            DP[i] = -1;
+        }
+
+        LISRecursive(A, DP, A.length);
+
+        return maxLIS;
     }
 }
