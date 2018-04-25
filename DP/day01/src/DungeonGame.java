e259f0e34e400245d78e8b1995457678e86377d3
Vicky McDermott <vickymmcd@gmail.com>
2018-04-25 10:35:00 -0400

    starting hw

--- DP/day01/src/DungeonGame.java
+++ DP/day01/src/DungeonGame.java
@@ -1,7 +1,42 @@
 public class DungeonGame {
+    // Subproblem: DP[i][j] = minInitialHealth if i,j is the starting point to end
+    // Guess: we go down (/up since im doing backwards)? we go right (/left since im doing backwards)?
+    // Recurrence Relation: Max(Min(going down, going right), 1)
+    // Memo/Recurse: DP[i][j] = Max(Min(going down, going right), 1)
+    // Solve final: return DP[0][0]
+    // runtime and space o(n^2) where n is length/width of map
+    // this is another bottom up approach
 
     public static int minInitialHealth(int[][] map) {
-        // TODO: Your code here
-        return 0;
+        int h = map.length;
+        int w = map[0].length;
+
+        int DP[][] = new int[h][w];
+
+        // bottom corner (destination)
+        if(map[h-1][w-1] > 0)
+            DP[h-1][w-1] = 1;
+        else
+            DP[h-1][w-1] = Math.abs(map[h-1][w-1]) + 1; //absolute value of the neg # +1
+
+        // fill in the last row and column making sure you never go below 1
+        // the dp of the previous one (closer to bottom right) - the current or 1 if 1 is
+        // greater than whatever that value is (never let fall below 1)
+        for (int i = h-2; i >= 0; i--)
+            DP[i][w-1] = Math.max(DP[i+1][w-1] - map[i][w-1], 1);
+        for (int j = w-2; j >= 0; j--)
+            DP[h-1][j] = Math.max(DP[h-1][j+1] - map[h-1][j], 1);
+
+        for (int i=h-2; i>=0; i--)
+        {
+            for (int j=w-2; j>=0; j--)
+            {
+                // move right or move down
+                int min = Math.min(DP[i+1][j], DP[i][j+1]);
+                DP[i][j] = Math.max(min - map[i][j], 1);
+            }
+        }
+
+        return DP[0][0];
     }
 }
--- DP/day01/src/EditDistance.java
+++ DP/day01/src/EditDistance.java
@@ -1,8 +1,46 @@
 public class EditDistance {
 
+    // Subproblem: DP[i][j] = permutations it takes to get from string a up to index i
+    // to string b up to index j
+    // Guess: we replace? we delete? we insert? we do nothing?
+    // Recurrence Relation: Min(deleting, inserting, replacing, doing nothing)
+    // Memo/Recurse: DP[i][j] = min(deleting, inserting, replacing)
+    // Solve final: return DP[a.length][b.length]
+    // runtime and space o(n^2) where n is string length
+    // this is my bottom up approach
+
     public static int minEditDist(String a, String b) {
-        // TODO: Your code here
-        return 0;
+        // have to be very cautious of off by one errors because when index
+        // is 0 we are assuming the string is empty string (not really true though
+        // making another variable for actual index of letter to make simpler
+        int index1;
+        int index2;
+        int[][] DP = new int[a.length()+1][b.length()+1];
+        for(int i=0; i<a.length()+1; i++){
+            for(int j=0; j<b.length()+1; j++){
+                index1 = i-1;
+                index2 = j-1;
+                if(i==0){
+                    DP[i][j] = j;
+                }
+                else if(j==0){
+                    DP[i][j] = i;
+                }
+                else{
+                    int delete = DP[i-1][j] + 1;
+                    int insert = DP[i][j-1] + 1;
+                    int replace;
+                    if(a.charAt(index1) == b.charAt(index2)){
+                        replace = DP[i - 1][j - 1];
+                    }
+                    else{
+                        replace = DP[i - 1][j - 1] + 1;
+                    }
+                    DP[i][j] = Math.min(delete, Math.min(insert, replace));
+                }
+            }
+        }
+        return DP[a.length()][b.length()];
     }
 
 }
